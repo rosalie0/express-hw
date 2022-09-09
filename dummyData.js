d33eb69c1e443c78a805238de8ecd46d560f3c00
Rosalie Newman <rosalienewman0@gmail.com>
2022-09-09 09:42:14 -0400

    first commit

---
+++ dummyData.js
@@ -0,0 +1,32 @@
+const data = [
+	{
+		title: 'The Lighthouse',
+		year: 2019,
+		director: 'Robert Eggers',
+		starring: ['Willem Dafoe', 'Robert Pattinson'],
+		runtime: 109,
+		blurb:
+			'Two lighthouse keepers try to maintain their sanity while living on a remote and mysterious New England island in the 1890s.',
+		trailerURL: 'https://www.youtube.com/watch?v=Hyag7lR8CPA',
+	},
+	{
+		title: 'Good Time',
+		year: 2017,
+		director: ['Josh Safdie', 'Benny Safdie'],
+		starring: ['Robert Pattinson'],
+		runtime: 101,
+		blurb:
+			"After a botched bank robbery lands his younger brother in prison, Connie Nikas embarks on a twisted odyssey through New York City's underworld to get his brother Nick out of jail.",
+		trailerURL: 'https://www.youtube.com/watch?v=upsR80YmwWc',
+	},
+	{
+		title: 'Pig',
+		year: 2021,
+		director: ['Michael Sarnoski'],
+		starring: ['Nicolas Cage', 'Alex Wolff', 'Adam Arkin'],
+		runtime: 92,
+		blurb:
+			'A truffle hunter who lives alone in the Oregon wilderness must return to his past in Portland in search of his beloved foraging pig after she is kidnapped.',
+		trailerURL: 'https://www.youtube.com/watch?v=-4nRpdONaAA',
+	},
+];
